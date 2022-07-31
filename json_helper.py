fd962ce367b1a8551af5ff0ec3f5703f3ca6f234
Payel Banerjee <payel.nandy2580@gmail.com>
2022-07-31 12:39:28 -0400

    lab9

--- json_helper.py
+++ json_helper.py
@@ -5,42 +5,56 @@ def read_json(filepath):
     f = open(filepath)
 
     return json.loads(f.read())
-path = "/Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/"
+file_path = "/Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json"
 # /Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/link.json
-data = "Payel"
-
+# data = "Payel"
+# print(read_json(file_path))
+path = "/Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/"
 
 
 # define a function
 # path to the directory is a string and is given
 # return a list containing json objects
 def read_all_json_files(filepath):
-    list = []
+    objects = []
+    # path, directories, files = list(os.walk(filepath))[0]
     for path, directories, files in os.walk(filepath):
+    # print(path,directories,files)
         for file in files:
             if '.json' in file:
                 jsonobject = read_json(filepath + file)
-                list.append(jsonobject)
-    return list
-print(read_all_json_files(path))
+                objects.append(jsonobject)
+    return objects
+data = read_all_json_files(path)
+
 
 # define function
 # take file path (given) and data(given)
 # write the contents of the json files to a file called **super_smash_characters.pickle**.
 # with open create a file at the given path with **super_smash_characters.pickle** name
 def write_pickle(filepath, data):
-    with open("super_smash_characters.pickle","wb") as f:
-        pickle.dumps(path,data,f)
-    with open("super_smash_characters.pickle","rb") as rf:
-        a = pickle.load(rf)
-        b = pickle.load(rf)
-    print(a)
-    print(b)
+    file = open(filepath + "super_smash_characters.pickle","wb")
+    pickle.dump(data,file)
+    file.close()
+
+write_pickle(path,data)
+# print (data)
+    # with open("super_smash_characters.pickle","wb") as f:
+    #     pickle.dumps(path,data,f)
+    # with open("super_smash_characters.pickle","rb") as rf:
+    #     a = pickle.load(rf)
+    #     b = pickle.load(rf)
+    # print(a)
+    # print(b)
 
 # write_pickle("/Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/")
 
 
 def load_pickle(filepath):
-    with open("super_smash_characters.pickle","rb")as f:
+
+    with open(filepath,"rb")as f:
         a = pickle.load(f)
-    print(a)
+
+    return a
+pickle_file_path =  "/Users/payel/Python/PythonFundamentals.Exercises.Part9/data/super_smash_bros/super_smash_characters.pickle"
+print (load_pickle(pickle_file_path))
